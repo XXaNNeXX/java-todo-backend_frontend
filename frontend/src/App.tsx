4c22b35cca22fcd716ca55dbe0d5879fef444c20
neuefische <info@neuefische.de>
2023-10-04 19:01:27 +0200

    fix addCard function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,32 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import Header from "./Header.tsx";
-import axios from "axios";
 import ToDoList from "./ToDoList.tsx";
 import {Route, Routes} from "react-router-dom";
 import {ToDo} from "./ToDo.tsx";
 import AddToDo from "./AddToDo.tsx";
+import axios from "axios";
 
 export default function App() {
 
   const [todo, setTodo] = useState<ToDo[]>([])
 
-  useEffect(() => loadTodo, [])
+    useEffect(() => {
+        axios.get("/api/todo")
+            .then(response => response.data)
+            .then(response => setTodo(response))
+            .catch(reason => console.error(reason.message))
+    }, [])
 
-  function loadTodo() {
-     axios.get("/api/todo")
-         .then((response) => setTodo(response.data))
-         .catch((reason) => console.error(reason.message))
-         .finally(() => console.log("Successful"))
-  }
-
-  function addCard(item: ToDo) {
-      setTodo([...todo, item])
-  }
+    function addCard(item: ToDo) {
+        axios.post("/api/todo", item)
+            .then(request1 => request1.data)
+            .then(request2 => setTodo([...todo, request2]))
+            /*.then(request=> {
+                setTodo(prevState => [...prevState, request.data])
+            })*/
+            .catch(reason => console.error(reason.message))
+    }
 
   return (
     <>
