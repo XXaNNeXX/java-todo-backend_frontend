0be226491d8aeb3ad9a272c291762e155f099880
neuefische <info@neuefische.de>
2023-10-05 18:22:58 +0200

    add deleteCard function

--- frontend/src/ToDoCard.tsx
+++ frontend/src/ToDoCard.tsx
@@ -1,15 +1,23 @@
 import {ToDo} from "./ToDo.tsx";
+import axios from "axios";
 
 type ToDoCard = {
-    todos: ToDo
+    todos: ToDo,
+    deleteCard: () => void
 }
 
 export default function ToDoCard(props: ToDoCard) {
+
+    function deleteCard() {
+        axios.delete("/api/todo/" + props.todos.id)
+            .then(props.deleteCard)
+    }
 
     return (
 
         <div className="todo-card">
             <p>{props.todos.description}</p>
+            <button onClick={deleteCard}>Delete</button>
         </div>
     )
 }
