287ebd10d8526f66875465fa99f03f1ed55afb42
neuefische <info@neuefische.de>
2023-10-05 18:23:32 +0200

    add deleteCard props to ToDoCard element

--- frontend/src/ToDoList.tsx
+++ frontend/src/ToDoList.tsx
@@ -1,19 +1,23 @@
 import ToDoCard from "./ToDoCard.tsx";
 import {ToDo} from "./ToDo.tsx";
+import {ToDoStatus} from "./ToDoStatus.tsx";
 
 type ToDoList = {
+    status: ToDoStatus,
     allTodos: ToDo[]
+    deleteCard: () => void
 }
 
 export default function ToDoList(props: ToDoList) {
 
     return (
 
-        <div>
-            <h1>My To Do List:</h1>
+        <div className="ToDoListColumn">
+            <h2>{props.status}</h2>
             {props.allTodos.map((todo => <ToDoCard
                     key={todo.id}
                     todos={todo}
+                    deleteCard={props.deleteCard}
             />))}
         </div>
     )
