0d1db93e5d311c32edccb01949341ffd62598410
neuefische <info@neuefische.de>
2023-10-08 17:48:58 +0200

    remove Links

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,14 +1,9 @@
-import {Link} from "react-router-dom";
-
 
 export default function Header() {
 
     return (
 
         <div className="header">
-            <Link to={"/"}>Open</Link>
-            <Link to={"/progress"}>In Progress</Link>
-            <Link to={"/done"}>Done</Link>
             <h1>My To Do List:</h1>
         </div>
     )
