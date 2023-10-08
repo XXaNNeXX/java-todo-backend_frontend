5876bc05c471970eb2fce3d5ea42840892c9faf6
neuefische <info@neuefische.de>
2023-10-08 17:50:00 +0200

    add move function and button onClick conditions

--- frontend/src/ToDoCard.tsx
+++ frontend/src/ToDoCard.tsx
@@ -1,6 +1,7 @@
 import {ToDo} from "./ToDo.tsx";
 import axios from "axios";
 import {ChangeEvent, useState} from "react";
+import {ToDoStatus} from "./ToDoStatus.tsx";
 
 type Props = {
     todo: ToDo,
@@ -25,11 +26,39 @@ export default function ToDoCard(props: Props) {
         } as ToDo)
     }
 
+    function move(targetStatus: ToDoStatus) {
+        axios.put("/api/todo/" + props.todo.id, {
+            ...props.todo,
+            status: targetStatus
+        } as ToDo)
+            .then(props.onItemChange)
+    }
+
+
     return (
 
         <div className="todo-card">
             <input value={text} onChange={changeTodo}/>
             <button onClick={deleteCard}>Delete</button>
+            {
+                props.todo.status === "OPEN"
+                    ? <div></div>
+                    : (
+                        props.todo.status === "IN_PROGRESS"
+                        ? <button onClick={() => move("OPEN")}>Prev</button>
+                        : <button onClick={() => move("IN_PROGRESS")}>Prev</button>
+                    )
+            }
+            {
+                props.todo.status === "DONE"
+                    ? <div></div>
+                    : (
+                        props.todo.status === "OPEN"
+                            ? <button onClick={() => move("IN_PROGRESS")}>Next</button>
+                            : <button onClick={() => move("DONE")}>Next</button>
+                    )
+            }
+
         </div>
     )
 }
