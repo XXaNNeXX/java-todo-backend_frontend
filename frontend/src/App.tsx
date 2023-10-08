faa00ddb742f49fddc954089574068d4f38d6aef
neuefische <info@neuefische.de>
2023-10-08 15:57:35 +0200

    remove addCard function and AddToDo element, change ToDoList attributes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./Header.tsx";
 import ToDoList from "./ToDoList.tsx";
 import {Route, Routes} from "react-router-dom";
 import {ToDo} from "./ToDo.tsx";
-import AddToDo from "./AddToDo.tsx";
 import axios from "axios";
 import {allPossibleTodos} from "./ToDoStatus.tsx";
 
@@ -12,34 +11,23 @@ export default function App() {
 
   const [todo, setTodo] = useState<ToDo[]>([])
 
-    function fetchData() {
+  function fetchData() {
         axios.get("/api/todo")
-            .then(response => response.data)
-            .then(response => setTodo(response))
+            .then(response=> setTodo(response.data))
             .catch(reason => console.error(reason.message))
-    }
-    useEffect(fetchData, [])
+  }
 
-    function addCard(item: ToDo) {
-        axios.post("/api/todo", item)
-            .then(response => response.data)
-            .then(response => setTodo([...todo, response]))
-            /*.then(request=> {
-                setTodo(prevState => [...prevState, request.data])
-            })*/
-            .catch(reason => console.error(reason.message))
-    }
+  useEffect(fetchData, [])
 
   return (
     <>
-     <Header/><br/>
-     <AddToDo addCard={addCard}/>
+        <Header/><br/>
         <div className="ToDoLists">
          <Routes>
             <Route path={"/"} element={
                 allPossibleTodos.map(status => {
                    const filteredTodos = todo.filter(todo => todo.status === status)
-                   return <ToDoList status={status} allTodos={filteredTodos} deleteCard={fetchData}/>
+                   return <ToDoList key={status} status={status} todos={filteredTodos} onItemChange={fetchData}/>
                 })}/>
          </Routes>
         </div>
