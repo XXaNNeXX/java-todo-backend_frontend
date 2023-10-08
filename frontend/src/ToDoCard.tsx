a4377d95d63e5c372f70636b59d6c6cc30e03e46
neuefische <info@neuefische.de>
2023-10-08 15:46:43 +0200

    add changeTodo function and change description <p> to <input>

--- frontend/src/ToDoCard.tsx
+++ frontend/src/ToDoCard.tsx
@@ -1,22 +1,34 @@
 import {ToDo} from "./ToDo.tsx";
 import axios from "axios";
+import {ChangeEvent, useState} from "react";
 
-type ToDoCard = {
-    todos: ToDo,
-    deleteCard: () => void
+type Props = {
+    todo: ToDo,
+    onItemChange: () => void
 }
 
-export default function ToDoCard(props: ToDoCard) {
+export default function ToDoCard(props: Props) {
+
+    const [text, setText] = useState(props.todo.description)
 
     function deleteCard() {
-        axios.delete("/api/todo/" + props.todos.id)
-            .then(props.deleteCard)
+        axios.delete("/api/todo/" + props.todo.id)
+            .then(props.onItemChange)
+    }
+
+    function changeTodo(event: ChangeEvent<HTMLInputElement>) {
+        const newDescription = event.target.value
+        setText(newDescription)
+        axios.put("/api/todo/" + props.todo.id, {
+            ...props.todo,
+            description: newDescription,
+        } as ToDo)
     }
 
     return (
 
         <div className="todo-card">
-            <p>{props.todos.description}</p>
+            <input value={text} onChange={changeTodo}/>
             <button onClick={deleteCard}>Delete</button>
         </div>
     )
