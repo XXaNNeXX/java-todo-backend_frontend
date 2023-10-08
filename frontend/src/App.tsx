f27a91ccb2a04fa5c61758702418cda9a4766cf5
neuefische <info@neuefische.de>
2023-10-08 17:50:33 +0200

    rename state variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,13 @@ import {useEffect, useState} from 'react'
 import './App.css'
 import Header from "./Header.tsx";
 import ToDoList from "./ToDoList.tsx";
-import {Route, Routes} from "react-router-dom";
 import {ToDo} from "./ToDo.tsx";
 import axios from "axios";
 import {allPossibleTodos} from "./ToDoStatus.tsx";
 
 export default function App() {
 
-  const [todo, setTodo] = useState<ToDo[]>([])
+  const [todos, setTodo] = useState<ToDo[]>([])
 
   function fetchData() {
         axios.get("/api/todo")
@@ -23,13 +22,12 @@ export default function App() {
     <>
         <Header/><br/>
         <div className="ToDoLists">
-         <Routes>
-            <Route path={"/"} element={
-                allPossibleTodos.map(status => {
-                   const filteredTodos = todo.filter(todo => todo.status === status)
-                   return <ToDoList key={status} status={status} todos={filteredTodos} onItemChange={fetchData}/>
-                })}/>
-         </Routes>
+        {
+            allPossibleTodos.map(status => {
+               const filteredTodos = todos.filter(todo => todo.status === status)
+               return <ToDoList key={status} status={status} todos={filteredTodos} onItemChange={fetchData}/>
+            })
+        }
         </div>
 
     </>
